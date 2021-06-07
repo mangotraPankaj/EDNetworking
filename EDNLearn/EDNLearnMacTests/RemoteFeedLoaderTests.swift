dcdbe0ba4f1935bff10ecbf37f93407ee1892ac0
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-07 13:29:04 +0530

    converting HTTPClient from shared instance to protocol for better control and apply SRP principle.

--- EDNLearn/EDNLearnMacTests/RemoteFeedLoaderTests.swift
+++ EDNLearn/EDNLearnMacTests/RemoteFeedLoaderTests.swift
@@ -8,34 +8,34 @@
 import XCTest
 
 
-class HTTPClient {
-    
-    
-    static var shared = HTTPClient()
-   // private init() {}
-    
-    func get(from url:URL)  {}
+protocol HTTPClient {
+    func get(from url:URL)
     
 }
 
 class RemoteFeedLoader {
+    let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
     func load() {
-        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
 class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
-     override func get(from url:URL) {
+      func get(from url:URL) {
         requestedURL = url
     }
 }
+
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        _ = RemoteFeedLoader()
+        _ = RemoteFeedLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
         
@@ -43,30 +43,9 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_requestDataFromURL() {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        let sut = RemoteFeedLoader()
+        let sut = RemoteFeedLoader(client: client)
         sut.load()
         
         XCTAssertNotNil(client.requestedURL)
     }
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
