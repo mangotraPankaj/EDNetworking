1a28f68ae40e88470e27fb20acd538fd3b863ec9
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-07 13:04:05 +0530

    Request data from URL load

--- EDNLearn/EDNLearnMacTests/RemoteFeedLoaderTests.swift
+++ EDNLearn/EDNLearnMacTests/RemoteFeedLoaderTests.swift
@@ -10,18 +10,35 @@ import XCTest
 
 class HTTPClient {
     var requestedURL: URL?
-}
-class RemoteFeedLoader {
     
+    static let shared = HTTPClient()
+    private init() {
+        
+    }
+    
+}
+
+class RemoteFeedLoader {
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
         
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
