5eb52034c3290785db2d24ebed05fc97d57e2e14
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-10 12:38:19 +0530

    Refactoring

--- EDNLearn/EDNLearnMacTests/RemoteFeedLoaderTests.swift
+++ EDNLearn/EDNLearnMacTests/RemoteFeedLoaderTests.swift
@@ -93,7 +93,9 @@ class RemoteFeedLoaderTests: XCTestCase {
         
         func complete(withStatusCode code: Int, at index: Int = 0)  {
             let response = HTTPURLResponse(url: requestedURLs[index],
-                                           statusCode: code, httpVersion: nil, headerFields: nil)!
+                                           statusCode: code,
+                                           httpVersion: nil,
+                                           headerFields: nil)!
             messages[index].completion(.success(response))
         }
     }
