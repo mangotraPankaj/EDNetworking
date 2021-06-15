ad76ad7452f27e03cf3489713a71563e9ba34619
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-15 11:35:33 +0530

    Moved HTTPClient to its own file.

---
+++ EDNLearn/EDNLearn/Feed Feature/HTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPClient.swift
+//  EDNLearn
+//
+//  Created by Pankaj Mangotra on 15/06/21.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
