c4b82f3b64306e9e92422f8af382e64292894d37
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-21 12:00:20 +0530

    made makeSUT factory method for creating URLSessionHTTPClient to protect our tests from breaking changes.

--- EDNLearn/EDNLearnMacTests/Feed API/URLSessionHTTPClientTests.swift
+++ EDNLearn/EDNLearnMacTests/Feed API/URLSessionHTTPClientTests.swift
@@ -33,9 +33,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
 
     func test_getFromURL_performGETRequestWithURL() {
-        
-        
-        
+     
         let url = URL(string: "http://any-url.com")!
         let exp = expectation(description: "Wait for request")
         URLProtocolStub.observeRequests { request in
@@ -44,7 +42,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url, completion: {_ in })
+        makeSUT().get(from: url, completion: {_ in })
         wait(for: [exp], timeout: 1.0)
         
     }
@@ -54,10 +52,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "any error", code: 1)
         
         URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
-        let sut = URLSessionHTTPClient()
         let exp = expectation(description: "Wait for completion")
          
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(recievedError as NSError):
                 XCTAssertEqual(recievedError, error)
@@ -70,6 +67,9 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     //MARK: - Helpers
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     
     private class URLProtocolStub: URLProtocol {
         
