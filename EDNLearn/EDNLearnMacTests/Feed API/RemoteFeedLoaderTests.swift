ed347da4e2900bb289fc13293db2aa8674236b47
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-21 12:06:58 +0530

    Extract memory leak tracking method to a shared extension of XCTestCase

--- EDNLearn/EDNLearnMacTests/Feed API/RemoteFeedLoaderTests.swift
+++ EDNLearn/EDNLearnMacTests/Feed API/RemoteFeedLoaderTests.swift
@@ -131,14 +131,6 @@ class RemoteFeedLoaderTests: XCTestCase {
         return .failure(error)
     }
     
-    private func trackForMemoryLeaks(_ instance: AnyObject,
-                                     file: StaticString = #filePath,
-                                     line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance,"Instance should be deallocated. Memory leak.",file: file,line: line)
-        }
-    }
-    
     private func makeItem(id: UUID,
                           description: String? = nil,
                           location: String? = nil,
--- EDNLearn/EDNLearnMacTests/Feed API/URLSessionHTTPClientTests.swift
+++ EDNLearn/EDNLearnMacTests/Feed API/URLSessionHTTPClientTests.swift
@@ -74,14 +74,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         return sut
     }
     
-    private func trackForMemoryLeaks(_ instance: AnyObject,
-                                     file: StaticString = #filePath,
-                                     line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance,"Instance should be deallocated. Memory leak.",file: file,line: line)
-        }
-    }
-
+    
     
     private class URLProtocolStub: URLProtocol {
         
