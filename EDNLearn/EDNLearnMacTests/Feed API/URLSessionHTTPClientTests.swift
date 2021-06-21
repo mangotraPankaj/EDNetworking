157d89216b63aaf94a088eeb0848392d581d2b99
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-21 12:23:01 +0530

    Delivers unexpected values error when data, response and error are all nil

--- EDNLearn/EDNLearnMacTests/Feed API/URLSessionHTTPClientTests.swift
+++ EDNLearn/EDNLearnMacTests/Feed API/URLSessionHTTPClientTests.swift
@@ -15,10 +15,14 @@ class URLSessionHTTPClient {
     init(session: URLSession = .shared) {
         self.session = session
     }
+    struct UnexpectedValuesRepresentation: Error {}
+    
     func get(from url: URL, completion:@escaping (HTTPClientResult)-> Void) {
         session.dataTask(with: url) { _, _, error in
             if let error = error {
                 completion(.failure(error))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
             }
         }.resume()
     }
@@ -44,8 +48,8 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         makeSUT().get(from: url, completion: {_ in })
         wait(for: [exp], timeout: 1.0)
-        
     }
+    
     func test_getFromURL_failsOnRequestError() {
       
         let error = NSError(domain: "any error", code: 1)
@@ -57,6 +61,24 @@ class URLSessionHTTPClientTests: XCTestCase {
             switch result {
             case let .failure(recievedError as NSError):
                 XCTAssertEqual(recievedError, error)
+            default:
+                XCTFail("Expected failure with error \(error), got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_getFromURL_failsOnAllNilValues() {
+        let error = NSError(domain: "any error", code: 1)
+        URLProtocolStub.stub(data: nil, response: nil, error: nil)
+        
+        let exp = expectation(description: "Wait for completion")
+         
+        makeSUT().get(from: anyURL()) { result in
+            switch result {
+            case .failure:
+                break
             default:
                 XCTFail("Expected failure with error \(error), got \(result) instead")
             }
