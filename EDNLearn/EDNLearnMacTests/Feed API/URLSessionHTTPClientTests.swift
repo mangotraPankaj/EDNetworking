2502a001a1b416d45f6b5b673f49be0575d0a8c7
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-24 11:30:45 +0530

    Make URLSessionHTTPClient conform to HTTPClient protocol

--- EDNLearn/EDNLearnMacTests/Feed API/URLSessionHTTPClientTests.swift
+++ EDNLearn/EDNLearnMacTests/Feed API/URLSessionHTTPClientTests.swift
@@ -9,7 +9,7 @@ import XCTest
 import EDNLearnMac
 
 
-class URLSessionHTTPClient {
+class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
     
     init(session: URLSession = .shared) {
@@ -101,7 +101,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     //MARK: - Helpers
     private func makeSUT(file: StaticString = #filePath,
-                         line: UInt = #line) -> URLSessionHTTPClient {
+                         line: UInt = #line) -> HTTPClient {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
