e5fec3a6ba5d56afff4ffccffc46205696322431
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-24 11:36:17 +0530

    Moved URLSessionHTTPClient to its own production code class

---
+++ EDNLearn/EDNLearn/Feed API/URLSessionHTTPClient.swift
@@ -0,0 +1,30 @@
+//
+//  URLSessionHTTPClient.swift
+//  EDNLearn
+//
+//  Created by Pankaj Mangotra on 24/06/21.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+  private struct UnexpectedValuesRepresentation: Error {}
+    
+    public func get(from url: URL, completion:@escaping (HTTPClientResult)-> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
